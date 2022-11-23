f097a9378fcc2020c598003b3fb1ca857d71433b
lugmaroc7 <115988204+lugmaroc7@users.noreply.github.com>
2022-11-23 17:14:56 +0100

    Add files via upload

---
+++ project/basic_php/index.php
@@ -0,0 +1,53 @@
+<?php
+
+$a = 1; 
+
+$b = "i am a string";
+
+$c = true;
+
+$d = null;
+
+
+const PI = 3.142;
+
+
+define('API','https://api.eazyalerts.com');
+
+// API.'posts';
+
+// print(PI);
+
+// echo $a;
+
+$num1 = 5;
+
+// $num1 /= 5;
+// $num1 += 5;
+
+$num1 -= 5;
+
+// print($num1);
+
+
+$string1 = "I am a fine";
+
+$string1 .= " boy";
+
+// $string2 = " boy";
+
+
+// $string3 = $string1 . $string2;
+
+
+print($string1);
+
+// $arr1 = [0 =>'one', 2=>'two',3=>'three'];
+// $arr2 = [0=>'one',0=>'two',"3"=>'three'];
+
+
+// print($arr1 == $arr2);
+
+// print($arr1 === $arr2);
+
+?>
